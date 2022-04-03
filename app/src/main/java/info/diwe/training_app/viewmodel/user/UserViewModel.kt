14918f9adfcab019ceeb6f15bfa7bfca6b18d9bc
Laurin49 <welter@diwe.info>
2022-04-03 11:10:42 +0200

    Kap14 Room with List Changes o.k.

--- app/src/main/java/info/diwe/training_app/viewmodel/user/UserViewModel.kt
+++ app/src/main/java/info/diwe/training_app/viewmodel/user/UserViewModel.kt
@@ -1,5 +1,6 @@
 package info.diwe.training_app.viewmodel.user
 
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import info.diwe.training_app.model.user.User
@@ -10,6 +11,24 @@ class UserViewModel(private val dao: UserDao): ViewModel() {
 
     var newUserName = ""
     var newUserEmail = ""
+
+    private val userList = dao.readAll()
+    val usersAsString = Transformations.map(userList) {
+        userList -> formatUserList(userList)
+    }
+
+    private fun formatUserList(userList: List<User>): String {
+        return userList.fold("") {
+            str, item -> str + '\n' + formatUser(item)
+        }
+    }
+
+    private fun formatUser(user: User): String {
+        var strUser = "ID: ${user.userId}"
+        strUser += " - " + "Name: ${user.userName}"
+        strUser += " - " + "Email: ${user.userEmail}" + '\n'
+        return strUser
+    }
 
     fun addUser() {
         viewModelScope.launch {
