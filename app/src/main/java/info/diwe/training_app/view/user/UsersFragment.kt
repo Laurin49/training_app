ff96ca363525e20efa42b0f8adce2b4c0a5be864
Laurin49 <welter@diwe.info>
2022-04-04 12:55:10 +0200

    Changes toolbar

--- app/src/main/java/info/diwe/training_app/view/user/UsersFragment.kt
+++ app/src/main/java/info/diwe/training_app/view/user/UsersFragment.kt
@@ -1,11 +1,12 @@
 package info.diwe.training_app.view.user
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.appbar.MaterialToolbar
 import info.diwe.training_app.R
 import info.diwe.training_app.databinding.FragmentUsersBinding
 import info.diwe.training_app.model.TrainingAppDatabase
@@ -24,10 +25,13 @@ class UsersFragment : Fragment() {
         _binding = FragmentUsersBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        setToolBarMenu(binding.toolbarUser)
+
         // Build the database, (if it doesn't already exist) and get a reference to the
         // userDao property
         val application = requireNotNull(this.activity).application
         val dao = TrainingAppDatabase.getInstance(application).userDao
+
 
         val viewModelFactory = UserViewModelFactory(dao)
         val viewModel = ViewModelProvider(this, viewModelFactory)
@@ -40,8 +44,25 @@ class UsersFragment : Fragment() {
         return view
     }
 
+    private fun setToolBarMenu(toolbarUser: MaterialToolbar) {
+        toolbarUser.inflateMenu(R.menu.user_toolbar)
+        // binding.toolbarUser.setNavigationIcon(resources.getDrawable(R.drawable.ic_action_back))
+        toolbarUser.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.addUserFragment -> {
+                    Toast.makeText(activity, "Add User Toolbar selected", Toast.LENGTH_LONG).show()
+                    true
+                }
+                else -> {
+                    true
+                }
+            }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
